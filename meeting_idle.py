f463da8645b0a381390c7d777a33591b3dc4beb6
Sahanakb <87218472+Sahanakb@users.noreply.github.com>
2021-07-11 13:34:21 +0530

    Add initial E-commerce website files

---
+++ meeting_idle.py
@@ -0,0 +1,9 @@
+first_number = 1+1+5
+print(first_number)
+second_number = 105+10
+print(second_number)
+
+total = first_number + second_number
+
+print(total)
+{"threads":[{"position":0,"start":0,"end":73,"connection":"open"},{"position":145,"start":74,"end":145,"connection":"closed"}],"url":"https://a.udemycdn.com/2018-07-09_00-04-22-6024d03a52d767eeeb12db11648ce8f9/original.py?nva=20200208153507&filename=meeting-idle.py&download=True&token=0dbbc1577fbb179135847","method":"GET","port":443,"downloadSize":145,"headers":{"date":"Tue, 06 Aug 2019 23:18:51 GMT","content-type":"application/force-download","content-length":"145","connection":"close","etag":"\"3ab0cbfcc467c74bdacc3f015499e9fd\"","last-modified":"Mon, 09 Jul 2018 00:04:23 GMT","server":"AmazonS3","x-amz-id-2":"KTNgHN59Cc5NjQoR76y5laU3VCkKbnkIHJmCwgfwqo356DrTow0ujGlS+ATIQD7kYu3CKldAeM4=","x-amz-meta-qqfilename":"meeting_idle.py","x-amz-replication-status":"COMPLETED","x-amz-request-id":"0C775BF0B5751251","x-amz-version-id":"_IvR8cg.3rRUn2wzRYdlNAe9KrXIm8P5","access-control-allow-origin":"*","age":"16026400","content-disposition":"attachment; filename=\"meeting-idle.py\"","accept-ranges":"bytes"}}
